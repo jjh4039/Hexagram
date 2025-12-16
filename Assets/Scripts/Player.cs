0ea4063e9e2647ae901295e7b25feb4ad7c2b1e4
정재하 <jjh4039@naver.com>
2025-12-16 23:45:16 +0900

    [Feat] WASD 이동, Idle Animation v.0.0.1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,30 +7,45 @@ public class Player : MonoBehaviour
     [SerializeField] private float moveSpeed = 5f; // 이동속도
 
     [Header("Components")]
-    private Rigidbody2D rigid;
+    [SerializeField] private Rigidbody2D rigid;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private PlayerInput inputActions;
 
+    [Header("Var")]
     [SerializeField] private Vector2 moveInput;
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>(); // 추가
+        inputActions = new PlayerInput();
 
-        rigid.gravityScale = 0; // 중력 제거 (탑뷰 최적화)
+        rigid.gravityScale = 0; // 중력 제거
         rigid.interpolation = RigidbodyInterpolation2D.Interpolate; // 프레임 끊김 방지
-    }   
+    }
 
-    public void OnMove(InputValue value)
+    private void OnEnable()
     {
-        moveInput = value.Get<Vector2>();
+        inputActions.Enable(); // 입력 활성화
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable(); // 입력 비활성화
+    }
+
+    private void Update()
+    {
+        moveInput = inputActions.Player.Move.ReadValue<Vector2>(); // 기초 이동 관련
+        Flip();
     }
 
     private void FixedUpdate()
     {
-        Move();
-      
+        Move(); // 기초 이동
     }
 
-    private void Move()
+    private void Move() // 기초 이동
     {
         if (moveInput.magnitude > 0)
         {
@@ -39,8 +54,19 @@ public class Player : MonoBehaviour
         }
         else
         {
-           // rb.linearVelocity = Vector2.zero;
+            rigid.linearVelocity = Vector2.zero;
+        }
+    }
 
+    private void Flip() // 캐릭터 좌우 반전
+    {
+        if (moveInput.x > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (moveInput.x < 0)
+        {
+            spriteRenderer.flipX = true; 
         }
     }
 }
