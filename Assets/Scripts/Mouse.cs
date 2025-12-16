2b7496ee33f198d8f38904919ae18964bf394e82
정재하 <jjh4039@naver.com>
2025-12-17 00:54:11 +0900

    [Feat] 임시 마우스 포인터 v.0.0.1

---
+++ Assets/Scripts/Mouse.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class VirtualCursor : MonoBehaviour
+{
+    private PlayerInput inputActions; // 아까 만든 에셋 클래스 이름
+
+    private void Awake()
+    {
+        inputActions = new PlayerInput();
+
+        // 실제 마우스 커서는 숨기기
+        Cursor.visible = false;
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Enable(); // 입력 활성화
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable(); // 입력 비활성화
+    }
+
+    private void Update()
+    {
+        // 마우스 위치 로직
+        Vector2 mouseScreenPos = inputActions.Player.Look.ReadValue<Vector2>();
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(new Vector3(mouseScreenPos.x, mouseScreenPos.y, 10f));
+
+        transform.position = worldPos;
+    }
+}
